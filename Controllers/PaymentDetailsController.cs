aaba460bbcc5ba35e605cfd49271fe79feb16b4a
Nithin Mathew <nithin.mathew@sysfore.com>
2024-02-05 18:35:54 +0530

    Payment API implementation

---
+++ Controllers/PaymentDetailsController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PaymentAPI.Models;
+
+namespace PaymentAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaymentDetailsController : ControllerBase
+    {
+        private readonly PaymentDetailContext _context;
+
+        public PaymentDetailsController(PaymentDetailContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/PaymentDetails
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<PaymentDetails>>> GetPaymentDetails()
+        {
+          if (_context.PaymentDetails == null)
+          {
+              return NotFound();
+          }
+            return await _context.PaymentDetails.ToListAsync();
+        }
+
+        // GET: api/PaymentDetails/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<PaymentDetails>> GetPaymentDetails(int id)
+        {
+          if (_context.PaymentDetails == null)
+          {
+              return NotFound();
+          }
+            var paymentDetails = await _context.PaymentDetails.FindAsync(id);
+
+            if (paymentDetails == null)
+            {
+                return NotFound();
+            }
+
+            return paymentDetails;
+        }
+
+        // PUT: api/PaymentDetails/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutPaymentDetails(int id, PaymentDetails paymentDetails)
+        {
+            if (id != paymentDetails.PaymentDetailsId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(paymentDetails).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PaymentDetailsExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return Ok(await _context.PaymentDetails.ToListAsync());
+        }
+
+        // POST: api/PaymentDetails
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<PaymentDetails>> PostPaymentDetails(PaymentDetails paymentDetails)
+        {
+          if (_context.PaymentDetails == null)
+          {
+              return Problem("Entity set 'PaymentDetailContext.PaymentDetails'  is null.");
+          }
+            _context.PaymentDetails.Add(paymentDetails);
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.PaymentDetails.ToListAsync());
+        }
+
+        // DELETE: api/PaymentDetails/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePaymentDetails(int id)
+        {
+            if (_context.PaymentDetails == null)
+            {
+                return NotFound();
+            }
+            var paymentDetails = await _context.PaymentDetails.FindAsync(id);
+            if (paymentDetails == null)
+            {
+                return NotFound();
+            }
+
+            _context.PaymentDetails.Remove(paymentDetails);
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.PaymentDetails.ToListAsync());
+        }
+
+        private bool PaymentDetailsExists(int id)
+        {
+            return (_context.PaymentDetails?.Any(e => e.PaymentDetailsId == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ Models/PaymentDetails.cs
@@ -0,0 +1,24 @@
+﻿using PaymentAPI.Models;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PaymentAPI.Models
+{
+    public class PaymentDetails
+    {
+        [Key]
+        public int PaymentDetailsId { get; set; }
+
+        [Column(TypeName ="nvarchar(100)")]
+        public string CardOwnerName { get; set; } = "";
+        [Column(TypeName = "nvarchar(100)")]
+        public string CardNumber { get; set; } = "";
+        [Column(TypeName = "nvarchar(100)")]
+        public string ExpirationDate { get; set; } = "";
+        [Column(TypeName = "nvarchar(100)")]
+        public string SecurityCode { get; set; } = "";     
+        
+    }
+    
+}
+
